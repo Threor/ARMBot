02ce49ea8a744cc27846f0f96207af845c8a4a72
Tobias.Markert <Tobias.Markert@vit17-061.vit.intern>
2019-07-28 22:55:01 +0200

    javadoc - created docs

--- src/de/arm/bot/model/Maze.java
+++ src/de/arm/bot/model/Maze.java
@@ -226,7 +226,9 @@ public class Maze {
      * Also calculates the distance using the wrap functionality if it is possible to wrap.
      * If it is possible to wrap on all for directions, five different distances will be calculated
      * Returned will be the lowest calculated distance
-     *
+     * 
+     * @param from The first cell 
+     * @param to The second cell
      * @return The calculated lowest distance
      */
     public int getDistance(Cell from, Cell to) {
--- src/de/arm/bot/model/PrimitiveStatus.java
+++ src/de/arm/bot/model/PrimitiveStatus.java
@@ -60,6 +60,7 @@ public enum PrimitiveStatus {
      * The default constructor for the enumeration
      *
      * @param navigable Indicates whether the bot can navigate over a cell or not
+     * @param cost The cost of walking on a cell with this status
      */
     PrimitiveStatus(boolean navigable, int cost) {
         this.navigable = navigable;
